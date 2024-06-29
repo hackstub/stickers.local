cace8fcfae2f9113e23255f39840c6f46f94f0e6
Alexandre Aubin <alex.aubin@mailoo.org>
2024-06-29 04:45:10 +0200

    Switch to didder for dithering

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -4,14 +4,31 @@ processing="$2"
 if [[ "$processing" == "dithering+resize+cropping" ]]
 then
     tmpdir=$(mktemp -d)
+    tmpdir2=$(mktemp -d)
     mv "$target" $tmpdir/
-    convert "$tmpdir/$(basename $target)" \
-        -background white \
-        -alpha remove \
-        -alpha off \
-        -contrast-stretch 5%x5% \
-        -colorspace gray \
-        -ordered-dither o8x8 \
+    #convert "$tmpdir/$(basename $target)" \
+    #    -background white \
+    #    -alpha remove \
+    #    -alpha off \
+    #    -contrast-stretch 5%x5% \
+    #    -colorspace gray \
+    #    -ordered-dither o8x8 \
+    #    -resize 991x \
+    #    -gravity Center \
+    #    -extent 991x306 \
+    #    "$target"
+
+    # From https://github.com/makew0rld/didder/releases
+    $(dirname $0)/didder_1.3.0_linux_64-bit \
+        --strength 70% \
+        --contrast 0.1 \
+        --palette "black white" \
+        --width 991 \
+        --input "$tmpdir/$(basename $target)" \
+        --ouput "$tmpdir2/$target" \
+        edm --serpentine FloydSteinberg
+
+    convert "$tmpdir2/$(basename $target)" \
         -resize 991x \
         -gravity Center \
         -extent 991x306 \
